0c97bb59901ca41853a36be89d496d0ee780511f
Loverde <w.andrew.loverde@gmail.com>
2023-11-02 12:30:42 -0500

    added import decorator

--- src/app/tavern-location/tavern-location.component.ts
+++ src/app/tavern-location/tavern-location.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TavernLocation } from '../tavern-location';
 
 @Component({
   selector: 'app-tavern-location',
