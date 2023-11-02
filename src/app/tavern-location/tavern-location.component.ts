1ecfe796aa43c6d2443c07c6d6733ddecc91a1be
Loverde <w.andrew.loverde@gmail.com>
2023-11-02 14:55:09 -0500

    corrected class naming error

--- src/app/tavern-location/tavern-location.component.ts
+++ src/app/tavern-location/tavern-location.component.ts
@@ -7,10 +7,10 @@ import { TavernLocation } from '../tavern-location';
   standalone: true,
   imports: [CommonModule],
   template: `
-<section class="listing">
-    <img class="listing-photo" [src]="tavernLocation.photo" alt="Exterior photo of {{tavernLocation.name}}">
-    <h2 class="listing-heading">{{ tavernLocation.name }}</h2>
-    <p class="listing-location">{{ tavernLocation.city}}, {{tavernLocation.kingdom }}</p>
+<section class="tavern">
+    <img class="tavern-photo" [src]="tavernLocation.photo" alt="Exterior photo of {{tavernLocation.name}}">
+    <h2 class="tavern-heading">{{ tavernLocation.name }}</h2>
+    <p class="tavern-location">{{ tavernLocation.city}}, {{tavernLocation.kingdom }}</p>
   </section>
   `,
   styleUrls: ['./tavern-location.component.css']
