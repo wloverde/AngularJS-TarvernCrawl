88b1c77a969aa5d9ff880380dbd2ad7bc6aafa17
Loverde <w.andrew.loverde@gmail.com>
2023-11-02 17:09:48 -0500

    integrated details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { TavernService } from '../tavern.service';
+import { TavernLocation } from '../tavern-location';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      details works!
-    </p>
+<article>
+    <img class="tavern-photo" [src]="tavernLocation?.photo"
+      alt="Exterior photo of {{tavernLocation?.name}}"/>
+    <section class="tavern-description">
+      <h2 class="tavern-heading">{{tavernLocation?.name}}</h2>
+      <p class="tavern-location">{{tavernLocation?.city}}, {{tavernLocation?.kingdom}}</p>
+    </section>
+    <section class="tavern-features">
+      <h2 class="section-heading">About this tavern location</h2>
+      <ul>
+        <li><h4>{{tavernLocation?.description}}</h4></li>
+        <li>Rooms available: {{tavernLocation?.rooms}}</li>
+        <li>Average Alignment of guests: {{tavernLocation?.alignment}}</li>
+      </ul>
+    </section>
+  </article>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  tavernService = inject(TavernService);
+  tavernLocation: TavernLocation | undefined;
+  
+  constructor() {
+    const tavernLocationId = Number(this.route.snapshot.params['id']);
+    this.tavernLocation = this.tavernService.getTavernLocationById(tavernLocationId);
+  }
 }
--- src/app/tavern-location/tavern-location.component.ts
+++ src/app/tavern-location/tavern-location.component.ts
@@ -1,21 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TavernLocation } from '../tavern-location';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tavern-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
-<section class="tavern">
-    <img class="tavern-photo" [src]="tavernLocation.photo" alt="Exterior photo of {{tavernLocation.name}}">
-    <h2 class="tavern-heading">{{ tavernLocation.name }}</h2>
-    <p class="tavern-location">{{ tavernLocation.city}}, {{tavernLocation.kingdom }}</p>
-  </section>
+    <a [routerLink]="['/details', tavernLocation.id]"
+      ><section class="tavern">
+        <img
+          class="tavern-photo"
+          [src]="tavernLocation.photo"
+          alt="Exterior photo of {{ tavernLocation.name }}"
+        />
+        <h2 class="tavern-heading">{{ tavernLocation.name }}</h2>
+        <p class="tavern-location">
+          {{ tavernLocation.city }}, {{ tavernLocation.kingdom }}
+        </p>
+      </section></a>
   `,
-  styleUrls: ['./tavern-location.component.css']
+  styleUrls: ['./tavern-location.component.css'],
 })
 export class TavernLocationComponent {
   @Input() tavernLocation!: TavernLocation;
 }
-
